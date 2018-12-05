52fe2e23104834984f5f694feafce5e2faaabf13
guanshida <fallenangel_gsd@163.com>
2018-12-05 20:41:27 +0800

    平方根

--- src/com/leetcode/binary_search/Sqrtx.java
+++ src/com/leetcode/binary_search/Sqrtx.java
@@ -22,12 +22,54 @@ package com.leetcode.binary_search;
 public class Sqrtx {
     public static void main(String[] args) {
         Sqrtx a = new Sqrtx();
-        System.out.println(a.mySqrt(9));
+        System.out.println(a.mySqrt2(Integer.MAX_VALUE, 1e-10));
+        System.out.println(a.mySqrt3(Integer.MAX_VALUE, 1e-10));
+        System.out.println(Math.sqrt(Integer.MAX_VALUE));
     }
     /**
+     * 扩展题：public double mySqrt(double x,double preci);
      * 解法一：使用jdk的默认函数
-     * 解法二：从1 到 x 使用二分查找找出目标数
+     * 解法二：从1 到 x 使用二分查找找出目标数，注意溢出。
      *      Time: O(log(n))     Space: O(1)
+     * 解法三：牛顿迭代法
+     *
+     * @param x
+     * @return
+     */
+
+    public double mySqrt2(double x,double preci) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        double start = 0,end = x;
+        double mid;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (Math.abs(mid - x / mid) < preci) {
+                return mid;
+            } else if (mid > x / mid) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        return -1;
+    }
+    public double mySqrt3(double x,double preci) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        double res = x;
+        while (Math.abs(res - x / res) >= preci) {
+            res = (res + x / res) / 2;
+        }
+
+        return res;
+    }
+
+    /**
+     * leetcode中的题。
      * @param x
      * @return
      */
@@ -38,17 +80,18 @@ public class Sqrtx {
         if (x < 4) {
             return 1;
         }
-        int start = 1;
+        int start = 0;
         int end = x;
         int mid;
         while (start <= end) {
             mid = (end + start) / 2;
-            if (mid * mid == x) {
+            long temp = mid * (long)mid;
+            if (temp == x) {
                 return mid;
-            } else if (mid * mid > x) {
+            } else if (temp > x) {
                 end = mid - 1;
             } else {
-                int mid1 = (mid + 1) * (mid + 1);
+                long mid1 = (long)(mid + 1) * (mid + 1);
                 if (mid1 == x) {
                     return mid + 1;
 
@@ -58,6 +101,8 @@ public class Sqrtx {
                 start = mid + 1;
             }
         }
+
         return -1;
     }
+
 }
